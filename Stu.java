079a90a7570be6f324a992a7bda8032f05ad0239
Harshitamiddha05 <145043751+Harshitamiddha05@users.noreply.github.com>
2025-02-19 00:00:50 +0530

    Add files via upload

---
+++ Stu.java
@@ -0,0 +1,11 @@
+public class Stu
+{
+ public static void main(String args[])
+ {
+  System.out.println("University rollno.:"+args[0]);
+  System.out.println("Name:"+args[1]);
+  System.out.println("Course:"+args[2]);
+  System.out.println("Semester:"+args[3]);
+  }
+}  
+  
