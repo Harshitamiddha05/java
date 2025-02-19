8fdd73de6617ee2bdb35f77f33c0be4a1e71ff7e
Harshitamiddha05 <145043751+Harshitamiddha05@users.noreply.github.com>
2025-02-19 23:49:44 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class fact
+{
+ public static void main(String args[])
+ {
+  int num;
+  int fac=1;
+  Scanner sc=new Scanner(System.in);
+  System.out.print("Enter the number:");
+  num=sc.nextInt();
+  for(int i=1;i<=num;i++)
+  {
+   fac*=i;
+  }
+  System.out.println("factorial of "+num+":"+fac);
+ }
+} 
---
+++ num.java
@@ -0,0 +1,15 @@
+public class num
+{
+ public static void main(String args[])
+ {
+  int n1=Integer.parseInt(args[0]);
+  int n2=Integer.parseInt(args[1]);
+  float n3=Float.parseFloat(args[2]);
+  float n4=Float.parseFloat(args[3]);
+  System.out.println("Addition:"+(n1+n2));
+  System.out.println("subtraction:"+(n1-n2));
+  System.out.println("Multiplication:"+(n3*n4));
+  System.out.println("Division:"+(n3/n4));
+ }
+}
+
